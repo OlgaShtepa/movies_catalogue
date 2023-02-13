5fe9f2f6967225f9a5c3320e37d45dbfd2e3e4e0
Olga Shtepa <olga.shtepa1@gmail.com>
2023-02-13 09:48:00 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import tmdb_client
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    movies = tmdb_client.get_movies(how_many=8)
+    return render_template("homepage.html", movies=movies, get_poster_url=tmdb_client.get_poster_url)
+
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
+
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    return render_template("homepage.html")
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ tmdb_client.py
@@ -0,0 +1,33 @@
+import requests
+
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1N2NmNjVmNmZiYjJhY2Y4YzY2M2I2ZjFiMzk0Mjc5NyIsInN1YiI6IjYzZTNhNWYyMTI4M2U5MDA4ZTQ4N2E5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a0GZEyPv7VTF9GimLwQGZeRFK2da-x5lWPxFszJnrMU"
+headers = {
+    "Authorization": f"Bearer {API_TOKEN}"
+}
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_TOKEN}"
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_popular_movies():
+    endpoint = "https://api.themoviedb.org/3/movie/popular"
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_movies(how_many):
+    data = get_popular_movies()
+    if "results" in data:
+        return data["results"][:how_many]
+    else:
+        return []
+
+def get_poster_url(poster_api_path, size="w342"):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{poster_api_path}"
+
+def get_movie_details(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_TOKEN}"
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
