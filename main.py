2c3f78c61ca8690681af6bd3ba102d043c7a09e9
Olga Shtepa <olga.shtepa1@gmail.com>
2023-02-15 08:25:45 +0100

    Fixed basic fanctionality

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template
+from django.core.checks import templates
+from flask import Flask, render_template, request, redirect, url_for
 import tmdb_client
+import random
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
+
 
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_movies(how_many=8)
-    return render_template("homepage.html", movies=movies, get_poster_url=tmdb_client.get_poster_url)
+    list_type = request.args.get('list_type', 'popular')
+    movies = tmdb_client.get_movies(how_many=8, list_type=list_type)
+    return render_template('homepage.html', movies=movies, current_list='popular')
+
+
+@app.route('/now_playing')
+def now_playing():
+    movies = tmdb_client.get_movies(how_many=8, list_type='now_playing')
+    return render_template('homepage.html', movies=movies, current_list='now_playing')
 
 
 @app.context_processor
@@ -19,9 +29,24 @@ def utility_processor():
 
 @app.route("/movie/<movie_id>")
 def movie_details(movie_id):
-    return render_template("homepage.html")
+    details = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_single_movie_cast(movie_id)
+    movie_images = tmdb_client.get_movie_images(movie_id)
+    selected_backdrop = random.choice(movie_images['backdrops'])
+    return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
+
+
+@app.route('/search')
+def search():
+    search_query = request.args.get('q', '')
+    if not search_query:
+        return redirect(url_for('homepage'))
+    movie = tmdb_client.search(search_query)
+    if not movie:
+        return render_template("no_results.html", search_query=search_query)
+    return redirect(url_for('movie_details', movie_id=movie.id))
 
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
